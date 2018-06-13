0d04f991498a3d8f53c2a4432f1ea9a6018dce0f
Mario Poljicanin <35070872+maverick8472@users.noreply.github.com>
2018-06-13 21:28:25 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 
+//Register on food2fork in order to get api access https://food2fork.com/about/api
+
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = 'aacc04aa6920816dd91c8e84ef80c7c2';
+export const key = 'use your own key from food2fork';
